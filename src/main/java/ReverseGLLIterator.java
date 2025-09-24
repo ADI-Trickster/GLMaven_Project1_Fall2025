0542d68763fd91a8d562fdb946539825cc7dfc2c
ADI_Anaya <anayaadrian121504@gmail.com>
2025-09-24 13:31:25 -0500

    started

--- src/main/java/ReverseGLLIterator.java
+++ src/main/java/ReverseGLLIterator.java
@@ -1,2 +1,12 @@
-public class ReverseGLLIterator {
+import java.util.Iterator;
+public class ReverseGLLIterator<T> implements Iterator<T> {
+
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    public T next(){
+        return  null;
+    }
 }
