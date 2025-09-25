2f640a5c4147d15ce308f46adcfa8754802fe74d
ADI_Anaya <anayaadrian121504@gmail.com>
2025-09-25 10:40:32 -0500

    commented what should be done

--- src/main/java/MyHashMap.java
+++ src/main/java/MyHashMap.java
@@ -7,12 +7,20 @@ public class MyHashMap <T> implements Iterable<T>{
 
     public MyHashMap(String key, T value){
         map = new ArrayList<GenericQueue>(10);
-        map.put(key,value);
+//        map.put(key,value);
     }
 
     public void put(String key, T value){
         int hash = key.hashCode();
-//        map[0].enqueue();
+        //use hash to decide index // nonneeded hash is index?
+        //use hash to check if There exist GQ at index for hash
+        if(map.get(hash) != null){
+            //add(value, hash)//add to the node at that index
+        }
+        else{// if not make new GQ and put at index
+            GenericQueue<T> queue = new GenericQueue<T>();
+            queue.add(value, hash);
+        }
     }
 
     public boolean contains(String key){
@@ -20,6 +28,9 @@ public class MyHashMap <T> implements Iterable<T>{
     }
 
     public T get(String key){
+        // return the value at the given key
+
+        //does not exist ret null
         return null;
     }
 
@@ -32,11 +43,13 @@ public class MyHashMap <T> implements Iterable<T>{
     }
 
     public T replace(String key, T value){
+        //
         return null;
     }
 
     public Iterator<T> iterator() {
-        return new HMIterator<T>();
+//        return new HMIterator<T>();
+        return null;
     }
 
 }
