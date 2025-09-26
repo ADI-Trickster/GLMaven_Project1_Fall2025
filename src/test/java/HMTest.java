615a24ed420b707b5d6ff036801d0a94a72444e2
ADI_Anaya <anayaadrian121504@gmail.com>
2025-09-25 20:23:58 -0500

    Added test TODO

--- src/test/java/HMTest.java
+++ src/test/java/HMTest.java
@@ -7,12 +7,14 @@ GenericQueue and MyHashMap and Node classes. Also test that the
 descendingIterator performs as expected.
  */
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+//import org.junit.jupiter.params.ParameterizedTest;
+//import org.junit.jupiter.params.provider.ValueSource;
+import java.util.Iterator;
+import java.util.ArrayList;
+
 
 public class HMTest {
 
@@ -23,19 +25,60 @@ public class HMTest {
 //        hashMap;
     }
     @BeforeEach
-    void init() {
+    void init(){
+        hashMap = new MyHashMap("test", 10);
+//        put();
+    }
+
+    @Test
+    void HasConstructorTest(){
+//        hashMap = new MyHashMap("test", 10);
+
+        assertEquals(1, hashMap.size(),"hashmap size is 1");
+        assertEquals(false, hashMap.isEmpty(),"hashmap is not empty");
+    }
+
+    @Test
+    void HashMapSizeTest() {
+//        assertEquals(1,MyHashMap.size(), "wrong length of hashMap");
+    }
+
+    @Test
+    void getKeyTest(){
+//        hashMap = new MyHashMap("test", 10);
+    }
+
+    @Test
+    public void getfuncTest() {
 
     }
 
-//    @Test
-//    void HashMapSizeTest() {
-////        assertEquals(0,MyHashMap.getLength(), "wrong length of hashMap");
-////        assertEquals(2,1, "false test");
-//    }
-//
-//    @Test
-//    public void test1Test() {
-//    }
+    @Test
+    void putTest(){
+
+    }
+
+    @Test
+    void containsKeyTest(){
+    }
+    @Test
+    void containsValueTest(){
+
+    }
+
+    @Test
+    void replaceKeyTest(){
+    }
+
+    @Test
+    void ForEveryKeyTest(){
+    }
+
+    @Test
+    void collisonTest(){
+    }
+
+
 
 //    @ParameterizedTest
 //    @ValueSource(ints = {1,2,3,4})
