439933dfbdede0138816cd9ad8c196b3667c1ca5
ADI_Anaya <anayaadrian121504@gmail.com>
2025-09-28 16:42:19 -0500

    Constuc and hasNext

--- src/main/java/HMIterator.java
+++ src/main/java/HMIterator.java
@@ -1,24 +1,47 @@
 import java.util.Iterator;
 import java.util.ArrayList;
 
-public class HMIterator<T> implements Iterator<T> {//TODO
+public class HMIterator<T> implements Iterator<T> {//TODO Finsish
     private HMIterator<T> iterator;
+    private ArrayList<GenericQueue<T>> map;
+    private GenericQueue<T> queue;
+    private GenericList.Node<T> curr;
+    private int index;
 
-    public HMIterator(ArrayList<GenericQueue<T>> map, int size) {
-//        this.iterator = iterator;
+    public HMIterator(ArrayList<GenericQueue<T>> map) {
+            this.map = map;
+            queue = map.get(0);
+            curr = queue.getHead();
+            index = 0;
     }
 
     @Override
     public boolean hasNext() {
-        return false;
+        if (curr == null && index < 10) {
+            index++;
+            if(index < 10){
+                queue = map.get(index);
+                while (queue == null) {
+                    index++;
+                    queue = map.get(index);
+                }
+                curr = queue.getHead();
+            }
+            else{
+                return false;
+            }
+        }
+        return curr != null;
     };
+
     @Override
     public T next() {
         if(!hasNext()){
             return null;
         }
-
-    return null;
+        T ret = curr.data;
+        curr = curr.next;
+    return ret;
     };
 
 }
