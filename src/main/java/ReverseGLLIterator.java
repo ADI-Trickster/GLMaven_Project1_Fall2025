589ca61b8732b23164c89a3436eef290b8090570
ADI_Anaya <anayaadrian121504@gmail.com>
2025-09-28 18:58:59 -0500

    ReverseGLLIterator

--- src/main/java/ReverseGLLIterator.java
+++ src/main/java/ReverseGLLIterator.java
@@ -1,22 +1,30 @@
 import java.util.Iterator;
+
 public class ReverseGLLIterator<T> implements Iterator<T> {//TODO
     private int index;
+    private GenericList<T> list;
 
     //don't use nodes use indexes for this
-    public ReverseGLLIterator() {
-
+    public ReverseGLLIterator(GenericList<T> head) {
+        list = head;
+        index = list.getLength() - 1;
     }
 
-
-//    @Override
+    //    @Override
     public boolean hasNext() {
+        if (index >= 0) {
+            return true;
+        }
         return false;
     }
 
-    public T next(){
+    public T next() {
         //check if it has next
-        T data = null;//get() data at index
-//        index--;
+        if(hasNext()){
+            return null;
+        }
+        T data = list.get(index);//get() data at index
+        index--;
         return data;
     }
 }
